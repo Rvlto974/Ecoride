dfcfa46acc253835dd486d283370d6f9966464eb
Rvlto974 <jacquet-m@protonmail.com>
2026-06-20 11:08:15 +0400

    Mise en place de la reecriture d'URL (front controller)

--- public/index.php
+++ public/index.php
@@ -1,3 +1,3 @@
 <?php
-echo '<h1>EcoRide fonctionne !</h1>';
-echo '<p>PHP version : ' . phpversion() . '</p>';
+echo '<h1>EcoRide</h1>';
+echo '<p>URL demandee : ' . htmlspecialchars($_SERVER['REQUEST_URI']) . '</p>';
